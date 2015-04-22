6ec17de6adf233a0e933dd84d8a0fef536b05622
David Caramelo <david.caramelo83@gmail.com>
2015-04-23 00:40:07 +0200

    Initial commit

---
+++ src/test/java/com/github/katalisthenics/BankTest.java
@@ -0,0 +1,14 @@
+package com.github.katalisthenics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BankTest {
+
+    @Test
+    public void test() {
+        int reponse = 42;
+        assertThat(reponse).isEqualTo(42);
+    }
+}
